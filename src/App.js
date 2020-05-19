fdf9f9afbb04e220a3f4a3a3ee479860243ec311
kappaswamy0195 <58642294+kappaswamy0195@users.noreply.github.com>
2020-05-19 11:09:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,134 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const api = {
+  key: '3374651b97eafa2915ed539ecdf3b09f',
+  base: 'https://api.openweathermap.org/data/2.5/',
+};
+
 function App() {
+  let [query, setQuery] = useState('');
+  let [weather, setWeather] = useState({});
+  let cssClassName = 'app rain';
+
+  const search = (evt) => {
+    if (evt.key === 'Enter') {
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery('');
+          cssClassName = getCSSClassName(result);
+          document.getElementById('container').className = cssClassName;
+          // document.getElementById('footer').className = 'footer special';
+          document.querySelectorAll('.footer').className = 'footer special';
+        });
+    }
+  };
+
+  const dateBuilder = (d) => {
+    let months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+    let days = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
+
+  let getCSSClassName = (weather) => {
+    console.log('In getCSS fn');
+    if (weather.weather == undefined) {
+      return 'error';
+    }
+    if (!weather.weather[0].main) {
+      return 'app';
+    }
+
+    console.log(weather.weather[0].main);
+
+    switch (weather.weather[0].main.toLowerCase()) {
+      case 'rain':
+        return 'app rain';
+
+      case 'haze':
+        return 'app haze';
+
+      case 'clouds':
+        return 'app clouds';
+
+      case 'mist':
+        return 'app mist';
+
+      default:
+        return 'app';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cssClassName} id="container">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Type city name and hit enter"
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
+            autoFocus
+          />
+        </div>
+        {weather.main ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+            <footer class="footer">
+              KnowWeather.com - Developed with passion by Karthik | Email:
+              <a href="mailto:aswamykarthik@gmail.com">
+                aswamykarthik@gmail.com
+              </a>
+            </footer>
+          </div>
+        ) : (
+          <footer class="footer">
+            Developed with passion by Karthik | Email:
+            <a href="mailto:aswamykarthik@gmail.com">aswamykarthik@gmail.com</a>
+          </footer>
+        )}
+      </main>
     </div>
   );
 }
